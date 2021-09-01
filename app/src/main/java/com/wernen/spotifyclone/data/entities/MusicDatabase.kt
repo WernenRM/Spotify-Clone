83330aa09231dda1b6a369e564e030b74e7c3fe3
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-01 14:13:51 -0300

    fun suspensa para pegar as msicas

--- app/src/main/java/com/wernen/spotifyclone/data/entities/MusicDatabase.kt
+++ app/src/main/java/com/wernen/spotifyclone/data/entities/MusicDatabase.kt
@@ -2,9 +2,18 @@ package com.wernen.spotifyclone.data.entities
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.wernen.spotifyclone.Constants.SONG_COLLECTION
+import kotlinx.coroutines.tasks.await
 
 class MusicDatabase {
 
     private val firestore = FirebaseFirestore.getInstance()
     private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
 }
