7fdbcebb8f06c6f9e357a53f29d0202ef71abeb4
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-02 19:36:09 -0300

    MusicPlayerNotificationListener.kt criado para gerenciar o ofechamento da notificação

--- app/src/main/java/com/wernen/spotifyclone/exoplayer/MusicService.kt
+++ app/src/main/java/com/wernen/spotifyclone/exoplayer/MusicService.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
+import callbacks.MusicPlayerNotificationListener
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -18,59 +19,66 @@ import javax.inject.Inject
 private const val SERVICE_TAG = "MusicService"
 
 @AndroidEntryPoint
-class MusicService: MediaBrowserServiceCompat() {
+class MusicService : MediaBrowserServiceCompat() {
 
-    @AndroidEntryPoint
-    class MusicService : MediaBrowserServiceCompat() {
+    @Inject
+    lateinit var dataSourceFactory: DefaultDataSourceFactory
 
-        @Inject
-        lateinit var dataSourceFactory: DefaultDataSourceFactory
+    @Inject
+    lateinit var exoPlayer: SimpleExoPlayer
 
-        @Inject
-        lateinit var exoPlayer: SimpleExoPlayer
+    private lateinit var musicNotificationManager: MusicNotificationManager
 
-        private val serviceJob = Job()
-        private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
-        private lateinit var mediaSession: MediaSessionCompat
-        private lateinit var mediaSessionConnector: MediaSessionConnector
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
 
-        override fun onCreate() {
-            super.onCreate()
-            val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
-                PendingIntent.getActivity(this, 0, it, 0)
-            }
+    var isForegroundService = false
 
-            mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
-                setSessionActivity(activityIntent)
-                isActive = true
-            }
-
-            sessionToken = mediaSession.sessionToken
-
-            mediaSessionConnector = MediaSessionConnector(mediaSession)
-            mediaSessionConnector.setPlayer(exoPlayer)
+    override fun onCreate() {
+        super.onCreate()
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
         }
 
-        override fun onDestroy() {
-            super.onDestroy()
-            serviceScope.cancel()
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
         }
 
-        override fun onGetRoot(
-            clientPackageName: String,
-            clientUid: Int,
-            rootHints: Bundle?
-        ): BrowserRoot? {
+        sessionToken = mediaSession.sessionToken
 
-        }
-
-        override fun onLoadChildren(
-            parentId: String,
-            result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+        musicNotificationManager = MusicNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
         ) {
 
         }
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
 
     }
 }
