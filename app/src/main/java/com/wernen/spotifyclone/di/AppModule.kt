2d66aa75931065528b74da4fcb1d66d0f421db5e
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-06 15:30:50 -0300

    configuração MainViewModel que está vinculado ao ciclo do MainActivity para que possa acessar o serviço a partir da atividade e fragmentos.

--- app/src/main/java/com/wernen/spotifyclone/di/AppModule.kt
+++ app/src/main/java/com/wernen/spotifyclone/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.wernen.spotifyclone.R
+import com.wernen.spotifyclone.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,16 +15,22 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
-class AppModule {
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
 
     @Singleton
     @Provides
     fun provideGlideInstance(
-      @ApplicationContext  context: Context
+        @ApplicationContext context: Context
     ) = Glide.with(context).setDefaultRequestOptions(
         RequestOptions()
-            .placeholder(R.drawable.common_full_open_on_phone)
-            .error(R.drawable.ic_launcher_background)
+            .placeholder(R.drawable.ic_baseline_image)
+            .error(R.drawable.ic_baseline_image)
             .diskCacheStrategy(DiskCacheStrategy.DATA)
     )
 
