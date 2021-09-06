3c84dc0c4ba8dc084e5a0b60fb1eed17e3033b5b
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-06 09:53:11 -0300

    Condição de erro qunado inicializado

--- app/src/main/java/com/wernen/spotifyclone/exoplayer/MusicService.kt
+++ app/src/main/java/com/wernen/spotifyclone/exoplayer/MusicService.kt
@@ -17,6 +17,7 @@ import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.wernen.spotifyclone.others.Constants.MEDIA_ROOT_ID
+import com.wernen.spotifyclone.others.Constants.NETWORK_ERROR
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import javax.inject.Inject
@@ -152,6 +153,7 @@ class MusicService : MediaBrowserServiceCompat() {
                             isPlayerInitialized = true
                         }
                     } else {
+                        mediaSession.sendSessionEvent(NETWORK_ERROR, null)
                         result.sendResult(null)
                     }
                 }
