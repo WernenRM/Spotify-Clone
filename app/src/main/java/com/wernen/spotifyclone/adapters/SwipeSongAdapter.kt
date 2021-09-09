7fc42daf8c8db7b0802a2b53f81f381449832ac3
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-09 14:11:46 -0300

    Criação do segundo Adapter para a info de uma musica especifica, utilizando herança

---
+++ app/src/main/java/com/wernen/spotifyclone/adapters/SwipeSongAdapter.kt
@@ -0,0 +1,27 @@
+package com.wernen.spotifyclone.adapters
+
+import androidx.recyclerview.widget.AsyncListDiffer
+import com.wernen.spotifyclone.R
+import com.wernen.spotifyclone.databinding.SwipeItemBinding
+
+class SwipeSongAdapter : BaseSongAdapter(R.layout.swipe_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+    private var _binding: SwipeItemBinding? = null
+    private val binding: SwipeItemBinding get() = _binding!!
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            val text = "${song.title} - ${song.subtitle}"
+            binding.tvPrimary.text = text
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+
+}
