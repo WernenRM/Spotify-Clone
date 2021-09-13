fb3d893a16968d85f3e3e2ff926ba9e140aae5df
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-13 10:27:42 -0300

    Limpando o projeto

--- app/src/main/java/com/wernen/spotifyclone/others/Constants.kt
+++ app/src/main/java/com/wernen/spotifyclone/others/Constants.kt
@@ -7,5 +7,6 @@ object Constants {
     const val NOTIFICATION_ID = 1
     const val MEDIA_ROOT_ID = "root_id"
     const val NETWORK_ERROR = "NETWORK_ERROR"
+    const val CHANEL_ID = "chanelId"
 
 }
