d1755538af68c492c7d90f56404a7380be4d9076
wernen.maciel <wernenrodrigues@gmail.com>
2021-09-14 17:42:20 -0300

    Segundo Adapter para substituir a Herança do anterior

---
+++ app/src/main/java/com/wernen/spotifyclone/adapters/SwipeSongAdapter2.kt
@@ -0,0 +1,50 @@
+package com.wernen.spotifyclone.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.wernen.spotifyclone.data.entities.Song
+import com.wernen.spotifyclone.databinding.SwipeItemBinding
+
+class SwipeSongAdapter2( val data: ArrayList<Song> = arrayListOf()) :
+    RecyclerView.Adapter<SwipeSongAdapter2.SwipeSongViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SwipeSongViewHolder {
+
+        val binding = SwipeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return SwipeSongViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: SwipeSongViewHolder, position: Int) {
+
+        val song = data[position]
+        holder.bind(song)
+    }
+
+    override fun getItemCount(): Int {
+        return data.count()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addall(data: ArrayList<Song>) {
+
+        this.data.addAll(data)
+
+        notifyDataSetChanged()
+    }
+
+
+    class SwipeSongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val binding = SwipeItemBinding.bind(itemView)
+
+        fun bind(song: Song) {
+
+            binding.tvPrimary.text =  song.title
+        }
+    }
+}
--- app/src/main/java/com/wernen/spotifyclone/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/wernen/spotifyclone/ui/fragments/HomeFragment.kt
@@ -1,13 +1,18 @@
 
 package com.wernen.spotifyclone.ui.fragments
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wernen.spotifyclone.R
-import com.wernen.spotifyclone.adapters.SongAdapter
+import com.wernen.spotifyclone.adapters.SongAdapter2
+import com.wernen.spotifyclone.adapters.SwipeSongAdapter
 import com.wernen.spotifyclone.databinding.FragmentHomeBinding
 import com.wernen.spotifyclone.others.Status
 import com.wernen.spotifyclone.ui.viewModel.MainViewModel
@@ -17,36 +22,58 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding: FragmentHomeBinding get() = _binding!!
+//    private var _binding: FragmentHomeBinding? = null
+//    private val binding: FragmentHomeBinding get() = _binding!!
+
+//    private val binding: FragmentHomeBinding by lazy {
+//
+//        FragmentHomeBinding.inflate(layoutInflater)
+//    }
+
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
     lateinit var mainViewModel: MainViewModel
 
     @Inject
-    lateinit var songAdapter: SongAdapter
+    lateinit var songAdapter: SongAdapter2
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        binding.rvAllSongs.adapter = songAdapter
         setupRecyclerView()
         subscribeToObservers()
 
         songAdapter.setItemClickListener {
             mainViewModel.playOrToggleSong(it)
+            findNavController().navigate(R.id.action_homeFragment_to_songFragment)
         }
     }
 
     private fun setupRecyclerView() = binding.rvAllSongs.apply {
         adapter = songAdapter
-        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun subscribeToObservers() {
+        Log.d("debug 2", "teste")
         mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
+            Log.d("debug 1", "$result")
             when(result.status) {
+
                 Status.SUCCESS -> {
                     binding.allSongsProgressBar.isVisible = false
                     result.data?.let { songs ->
-                        songAdapter.songs = songs
+                        songAdapter.addall(songs.toCollection(ArrayList()))
+                        Log.d("debug 1", "$songs")
                     }
                 }
                 Status.ERROR -> Unit
